e8e969b2df67730a04546da9f591b4347643e434
BrianProgramer <Briandanielprogramer@hotmail.com>
2022-11-08 00:53:19 -0600

    inserta Mensaje aquí

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState,useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 export default function App() {
+  //permisos
+  const [HasPermisos,SetHasPermisos]=useState(null);
+  //escaner
+  const [escaner,setEscaner]=useState(false);
+
+
+  useEffect(()=>{
+    const getEscanerpermisos=async ()=>{
+    const {status}=await BarCodeScanner.requestPermissionsAsync();
+    SetHasPermisos(status==='granted');
+  };
+  getEscanerpermisos();
+},[]);
+
+
+const conductorescaner=({type,data})=>{
+  setEscaner(true);
+  alert(`Codigo ${type} y el dato ${data}`);
+};
+
+if(HasPermisos==null){
+return <Text>rr camara </Text>
+}
+if (HasPermisos==false) {
+  return <Text>No permisos </Text>
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+      onBarCodeScanned={escaner? undefined: handleBarCodeScanned}
+      />
+      {escaner && <Button title={'Escanear'} onPress={()=>setEscaner(false)}/>}
     </View>
   );
 }
